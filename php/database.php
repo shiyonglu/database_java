a20238228e280307694a6b04dd7e2c04a7d30555
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2023-12-27 23:49:48 -0500

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,13 +1,3 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Names List</title>
-</head>
-<body>
-
-<h2>Names Table</h2>
-
 <?php
 $servername = "localhost";
 $username = "john";
@@ -22,21 +12,77 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM names";
-$result = $conn->query($sql);
+// Handle delete action
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
+    $idToDelete = $_GET['id'];
+    $deleteSql = "DELETE FROM names WHERE id = ?";
+    $deleteStmt = $conn->prepare($deleteSql);
+    $deleteStmt->bind_param("i", $idToDelete);
 
-if ($result->num_rows > 0) {
-    echo "<table border='1'><tr><th>ID</th><th>Name</th><th>Date Added</th></tr>";
-    while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["date_added"]."</td></tr>";
+    if ($deleteStmt->execute()) {
+        echo "Record with ID $idToDelete deleted successfully.";
+    } else {
+        echo "Error deleting record: " . $deleteStmt->error;
     }
-    echo "</table>";
-} else {
-    echo "0 results";
+
+    $deleteStmt->close();
 }
 
-$conn->close();
+// Fetch records from the database
+$sql = "SELECT * FROM names";
+$result = $conn->query($sql);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Database</title>
+</head>
+<body>
+
+<h2>Names Table</h2>
+
+<table border='1'>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Date Added</th>
+        <th>Action</th>
+        <th>Update</th>
+    </tr>
+    <?php
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $id = $row["id"];
+            $name = $row["name"];
+            $dateAdded = $row["date_added"];
+    ?>
+    <tr>
+        <td><?php echo $id; ?></td>
+        <td><?php echo $name; ?></td>
+        <td><?php echo $dateAdded; ?></td>
+        <td>
+            <form action="delete.php" method="get">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <input type="hidden" name="action" value="delete">
+                <button type="submit">Delete</button>
+            </form>
+        </td>
+        <td>
+            <form action="update.php" method="get">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <input type="hidden" name="action" value="update">
+                <button type="submit">Update</button>
+            </form>
+        </td>
+    </tr>
+    <?php
+        }
+    } else {
+        echo "<tr><td colspan='5'>0 results</td></tr>";
+    }
+    ?>
+</table>
 
 <br>
 <br>
@@ -51,3 +97,8 @@ $conn->close();
 
 </body>
 </html>
+
+<?php
+// Close the database connection
+$conn->close();
+?>
