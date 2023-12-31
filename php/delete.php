d9e7e254651b6408c16c31f0b760c3b7ac21ac2f
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2023-12-31 00:15:32 -0500

    Update delete.php

--- php/delete.php
+++ php/delete.php
@@ -13,25 +13,25 @@ if ($conn->connect_error) {
 }
 
 // Handle delete action
-if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
-    $id = $_GET['id'];
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['action'] == 'delete' && isset($_POST['id'])) {
+    $idToDelete = $_POST['id'];
     $deleteSql = "DELETE FROM names WHERE id = ?";
     $deleteStmt = $conn->prepare($deleteSql);
-    $deleteStmt->bind_param("i", $id);
+    $deleteStmt->bind_param("i", $idToDelete);
 
     if ($deleteStmt->execute()) {
-        echo "Record with ID $id deleted successfully.";
+        echo "Record with ID $idToDelete deleted successfully.";
     } else {
         echo "Error deleting record: " . $deleteStmt->error;
     }
 
     $deleteStmt->close();
+
+    // Redirect back to database.php
+    header("Location: database.php");
+    exit();
 }
 
-// Redirect back to database.php
-header("Location: database.php");
-exit();
-
+// Close the database connection
 $conn->close();
 ?>
-
